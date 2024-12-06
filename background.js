04307fcb9d2e3560029e08c4b735f03301256bf3
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2024-12-05 22:33:05 -0500

    Works pretty well now.
    Need to make it persistent across page navigation.

---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+  
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostContains: 'amazon.' },
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()]
+      }
+    ]);
+  });
+});
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "_execute_action") {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab?.url?.includes('amazon.')) {
+      chrome.action.openPopup();
+    }
+  }
+});
